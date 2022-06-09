d2df956f30066e3be96f04a14e4bd28ffecf3a34
eulucasm <lucaspoletis@gmail.com>
2022-06-08 22:59:28 -0300

    Aula 1 - Herança

---
+++ app/src/main/java/br/com/lucao/ebac/polimorfismo/aulaUm/PersonAndPlayer.kt
@@ -0,0 +1,33 @@
+package br.com.lucao.ebac.polimorfismo.aulaUm
+
+open class Person {
+   var name: String = ""
+   var email: String = ""
+
+   override fun toString() = "nome: $name email: $email"
+}
+
+class Player : Person() {
+   var number: Int = 0
+   override fun toString() = super.toString() + "numero: ${this.number}"
+}
+
+
+fun main() {
+
+   var person = Person()
+   person.name = "Lucas"
+   person.email = "email@mail.com"
+
+   var player = Player()
+   player.number = 99
+   player.name = "Jordan"
+   player.email = "mail@email.com"
+   println(player)
+
+   if(person is Person){
+      println("é uma pessoa")
+   }else if(player is Player){
+      println("é Jogador")
+   }
+}
